3bcb9fab0fd8c9046e18a29cd14b41fe6761693c
yonihalevi <yoni.halevi@gmail.com>
2021-07-12 20:43:40 +0300

    trying large model with smaller batch and more epochs

--- TrainGrozi.py
+++ TrainGrozi.py
@@ -4,8 +4,7 @@ import train
 
 def main():
     gp.etl()
-    train.run(imgsz=3264, batch=16, epochs=5, data="grozi.yaml", weights="yolov5s.pt")
-
+    train.run(imgsz=3264, batch=4, epochs=10, data="grozi.yaml", weights="yolov5l.pt")
 
 if __name__ == "__main__":
     main()
